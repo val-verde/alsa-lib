eca847e6d24b44e55d8b15f4036ec7e443c5a1f9
Aaditya Chandrasekhar <aadityac@gmail.com>
2021-03-31 23:42:56 -0700

    Fix ANDROID -> __ANDROID.

--- src/ucm/parser.c
+++ src/ucm/parser.c
@@ -2299,7 +2299,7 @@ int uc_mgr_scan_master_configs(const char **_list[])
 		snprintf(filename, sizeof(filename), "%s/ucm2",
 			 snd_config_topdir());
 
-#if defined(_GNU_SOURCE) && !defined(__NetBSD__) && !defined(__FreeBSD__) && !defined(__sun) && !defined(ANDROID)
+#if defined(_GNU_SOURCE) && !defined(__NetBSD__) && !defined(__FreeBSD__) && !defined(__sun) && !defined(__ANDROID__)
 #define SORTFUNC	versionsort
 #else
 #define SORTFUNC	alphasort
